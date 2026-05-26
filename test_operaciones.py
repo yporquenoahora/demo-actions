64a50b4b04f91a81961e3d9f5be71a81574b1a71
aidamarquez-max <aida.marquez@cruzroja.es>
2026-05-26 14:24:34 +0200

    real change

--- test_operaciones.py
+++ test_operaciones.py
@@ -16,6 +16,7 @@ class TestOperaciones(unittest.TestCase):
         self.assertEqual(restar(3, 2), 1)
         self.assertEqual(restar(-1, 1), -2)
         self.assertEqual(restar(-1, -1), 0)
+        self.assertEqual(restar(-1, -5), 4)
 
     def test_multiplicar(self):
         self.assertEqual(multiplicar(3, 2), 6)
